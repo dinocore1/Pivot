c4a9a3037b3d8192ac2e98a3be12aa3fd523270c
Paul Soucy <paul@dev-smart.com>
2016-12-25 19:30:05 -0500

    optional

--- include/pivot/Optional.h
+++ include/pivot/Optional.h
@@ -7,9 +7,12 @@ template<typename T>
 class Optional {
 public:
   Optional();
+  ~Optional();
   Optional(const T& obj);
 
   bool isInitialized() const;
+  operator bool() const;
+  void clear();
   T& get();
   T& operator= (const T&);
   operator T&();
@@ -25,6 +28,11 @@ Optional<T>::Optional()
 }
 
 template<typename T>
+Optional<T>::~Optional() {
+	clear();
+}
+
+template<typename T>
 Optional<T>::Optional(const T& obj) {
   get() = obj;
   mInitialized = true;
@@ -33,6 +41,19 @@ Optional<T>::Optional(const T& obj) {
 template<typename T>
 bool Optional<T>::isInitialized() const {
 	return mInitialized;
+}
+
+template<typename T>
+Optional<T>::operator bool() const {
+	return mInitialized;
+}
+
+template<typename T>
+void Optional<T>::clear() {
+	if (mInitialized) {
+		reinterpret_cast<T*>(mData)->~T();
+		mInitialized = false;
+	}
 }
 
 template<typename T>
@@ -50,7 +71,7 @@ T& Optional<T>::operator= (const T& obj) {
 
 template<typename T>
 Optional<T>::operator T&() {
-	return reinterpret_cast<T*>(mData);
+	return *reinterpret_cast<T*>(mData);
 }
 
 } // namespace pivot
