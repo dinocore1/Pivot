0ee84591c896b3ff27a1367efb4f7b3a752e87a6
paul <psoucy@sciaps.com>
2016-12-27 14:39:24 -0500

    fixing cmake build

--- src/config.h.in.cmake
+++ src/config.h.in.cmake
@@ -1,6 +1,9 @@
 #ifndef PIVOT_CONFIG_H
 #define PIVOT_CONFIG_H
 
+#define Pivot_VERSION_MAJOR @Pivot_VERSION_MAJOR@
+#define Pivot_VERSION_MINOR @Pivot_VERSION_MINOR@
+
 /////// User-config part for portable compiling
 
 #define OS_TYPE_UNIX ${UNIX}
